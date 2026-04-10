981cc9ca93e80086954dc1b3e988c041a611b34e
rawan hesham <99086753+rawan2610@users.noreply.github.com>
2026-04-10 04:22:01 +0200

    Add PrepDb class for database population
    Implement database seeding functionality in PrepDb class.

---
+++ Data/PrepDb.cs
@@ -0,0 +1,27 @@
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if(!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+            }
+            else
+            {
+                Console.Writeline("--> we already have data");
+            }
+
+        }
+    }
+}
